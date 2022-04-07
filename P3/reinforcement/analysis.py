ece5010622f6ffbd9112242bb62a54d61fd487d7
Nisarg Patel <patel321nisarg@gmail.com>
2022-04-07 01:29:19 -0400

    P3: Q8 completed

--- P3/reinforcement/analysis.py
+++ P3/reinforcement/analysis.py
@@ -24,12 +24,14 @@ def question2():
     answerNoise = 0.01
     return answerDiscount, answerNoise
 
+
 def question3a():
     answerDiscount = 0.1
     answerNoise = 0.001
     answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3b():
     answerDiscount = 0.1
@@ -38,12 +40,14 @@ def question3b():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3c():
     answerDiscount = 0.9
     answerNoise = 0.001
     answerLivingReward = -0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3d():
     answerDiscount = 0.9
@@ -52,6 +56,7 @@ def question3d():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3e():
     answerDiscount = 0.9
     answerNoise = 0.001
@@ -59,15 +64,18 @@ def question3e():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question8():
     answerEpsilon = None
     answerLearningRate = None
     return "NOT POSSIBLE"
     # If not possible, return 'NOT POSSIBLE'
 
+
 if __name__ == '__main__':
     print('Answers to analysis questions:')
     import analysis
+
     for q in [q for q in dir(analysis) if q.startswith('question')]:
         response = getattr(analysis, q)()
         print('  Question %s:\t%s' % (q, str(response)))
